e32e27fd17da802cf27bb5d6de6f31dfbe89e226
jordan <jordanfun0@gmail.com>
2021-01-03 22:32:28 -0800

    Added more functionality to the Board.java and BoardTest.java files to check game end conditions

--- src/main/Board.java
+++ src/main/Board.java
@@ -2,9 +2,13 @@ package main;
 
 public class Board {
     Tile[][] board;
+    Boolean XTurn;
+    Boolean gameIsOver;
 
     public Board() {
         board = new Tile[3][3];
+        XTurn = true;
+        gameIsOver = false;
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
@@ -13,8 +17,108 @@ public class Board {
         }
     }
 
-    public Tile getTile(int row, int column) {
-        return board[row][column];
+    public Tile getTile(int column, int row) {
+        return board[column][row];
     }
 
+    public Boolean getGameIsOver() {
+        return gameIsOver;
+    }
+
+    // EFFECTS: Marks a tile with X or O depending on who's turn it is, then switches turns
+    public Boolean markTile(int column, int row) {
+        if ((row <= 2 && row >= 0 && column <= 2 && column >= 0) || !gameIsOver) {
+            Tile currTile = getTile(column, row);
+            if (currTile.getState() == Tile.State.EMPTY) {
+                if (XTurn) {
+                    currTile.setState(Tile.State.X);
+                } else {
+                    currTile.setState(Tile.State.O);
+                }
+                XTurn = !XTurn;
+            }
+            gameIsOver = checkWin(column, row);
+            return gameIsOver;
+        }
+        return false;
+    }
+
+    // EFFECTS: Checks if there is a win at (column, row)
+    public Boolean checkWin(int column, int row) {
+        Boolean diagonals = false;
+        Boolean straights = checkStraights(column, row);
+
+        // if tile is in a corner or tile is in the center check diagonals for a win
+        if ((column != 1 && row != 1) || ((column == 1 && row == 1))) {
+            diagonals = checkDiagonals(column, row);
+        }
+
+        return diagonals || straights;
+    }
+
+    // EFFECTS: Checks if there is a win in horizontal or vertical directions at (column, row)
+    public Boolean checkStraights(int column, int row) {
+        return checkHorizontal(row) || checkVertical(column);
+    }
+
+    // EFFECTS: Checks if tiles in a row (horizontally) are all of the same mark
+    public Boolean checkHorizontal(int row) {
+        Tile tile1 = getTile(0, row);
+        Tile tile2 = getTile(1, row);
+        Tile tile3 = getTile(2, row);
+
+        if (tile1.getState() != Tile.State.EMPTY) {
+            return tile1.getState() == tile2.getState() && tile2.getState() == tile3.getState();
+        }
+        return false;
+    }
+
+    // EFFECTS: Checks if tiles in a column (vertically) are all of the same mark
+    public Boolean checkVertical(int column) {
+        Tile tile1 = getTile(column, 0);
+        Tile tile2 = getTile(column, 1);
+        Tile tile3 = getTile(column, 2);
+
+        if (tile1.getState() != Tile.State.EMPTY) {
+            return tile1.getState() == tile2.getState() && tile2.getState() == tile3.getState();
+        }
+        return false;
+    }
+
+    public Boolean checkDiagonals(int column, int row) {
+        Boolean downwards = false;
+        Boolean upwards = false;
+
+        if (column != row) {
+            upwards = checkUpwardsDiagonal();
+        } else if (column == 1) {
+            upwards = checkUpwardsDiagonal();
+            downwards = checkDownwardsDiagonal();
+        } else {
+            downwards = checkDownwardsDiagonal();
+        }
+        return downwards || upwards;
+    }
+
+    public Boolean checkDownwardsDiagonal() {
+        Tile tile1 = getTile(0, 0);
+        Tile tile2 = getTile(1, 1);
+        Tile tile3 = getTile(2, 2);
+
+        if (tile1.getState() != Tile.State.EMPTY) {
+            return tile1.getState() == tile2.getState() && tile2.getState() == tile3.getState();
+        }
+        return false;
+    }
+
+    public Boolean checkUpwardsDiagonal() {
+        Tile tile1 = getTile(2, 0);
+        Tile tile2 = getTile(1, 1);
+        Tile tile3 = getTile(0, 2);
+
+        if (tile1.getState() != Tile.State.EMPTY) {
+            return tile1.getState() == tile2.getState() && tile2.getState() == tile3.getState();
+        }
+        return false;
+    }
 }
--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
     Board board1;
+    Tile tile1;
 
     @BeforeEach
     void runBefore() {
@@ -18,8 +19,86 @@ public class BoardTest {
 
     @Test
     void testGetter() {
-        Tile tile1 = board1.getTile(0,0);
+        tile1 = board1.getTile(0,0);
         assertEquals(State.EMPTY, tile1.getState());
+    }
+
+    @Test
+    void testGetGameIsOver() {
+        assertFalse(board1.getGameIsOver());
+    }
+
+    @Test
+    void testMark() {
+        board1.getTile(1,0).setState(Tile.State.X);
+        board1.getTile(2,0).setState(Tile.State.O);
+
+        board1.markTile(0, 0);
+        board1.markTile(1, 0);
+        board1.markTile(2, 0);
+
+        assertEquals(State.X, board1.getTile(0, 0).getState());
+        assertEquals(State.X, board1.getTile(1, 0).getState());
+        assertEquals(State.O, board1.getTile(2, 0).getState());
+    }
+
+    @Test
+    void testMultipleMarks() {
+        board1.markTile(0, 0);
+        board1.markTile(1, 0);
+        board1.markTile(2, 0);
+
+        assertEquals(State.X, board1.getTile(0, 0).getState());
+        assertEquals(State.O, board1.getTile(1, 0).getState());
+        assertEquals(State.X, board1.getTile(2, 0).getState());
+    }
+
+    @Test
+    void testWinDownwards() {
+        assertFalse(board1.markTile(0, 0));
+        assertFalse(board1.markTile(1, 0));
+        assertFalse(board1.markTile(1, 1));
+        assertFalse(board1.markTile(2, 0));
+        assertTrue(board1.markTile(2, 2));
+        assertFalse(board1.checkStraights(2, 2));
+        assertTrue(board1.checkDiagonals(2, 2));
+        assertTrue(board1.checkDownwardsDiagonal());
+        assertTrue(board1.getGameIsOver());
+    }
+
+    @Test
+    void testWinUpwards() {
+        assertFalse(board1.markTile(0, 2));
+        assertFalse(board1.markTile(1, 0));
+        assertFalse(board1.markTile(1, 1));
+        assertFalse(board1.markTile(2, 2));
+        assertTrue(board1.markTile(2, 0));
+        assertFalse(board1.checkStraights(2, 0));
+        assertTrue(board1.checkDiagonals(2, 0));
+        assertTrue(board1.checkUpwardsDiagonal());
+    }
+    @Test
+    void testWinHorizontal() {
+        assertFalse(board1.markTile(0, 0));
+        assertFalse(board1.markTile(0, 1));
+        assertFalse(board1.markTile(1, 0));
+        assertFalse(board1.markTile(1, 1));
+        assertTrue(board1.markTile(2, 0));
+        assertTrue(board1.checkStraights(2, 0));
+        assertFalse(board1.checkDiagonals(2, 0));
+        assertTrue(board1.checkHorizontal(0));
+    }
+
+    @Test
+    void testWinVertical() {
+        assertFalse(board1.markTile(0, 0));
+        assertFalse(board1.markTile(1, 0));
+        assertFalse(board1.markTile(0, 1));
+        assertFalse(board1.markTile(1, 1));
+        assertTrue(board1.markTile(0, 2));
+        assertTrue(board1.checkStraights(0, 2));
+        assertFalse(board1.checkDiagonals(0, 2));
+        assertTrue(board1.checkVertical(0));
     }
 
 }
